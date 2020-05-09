3d5034492e411415988b401ea94d0cc9723c46b2
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-05-09 15:07:14 -0400

    fonts

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,47 +1,21 @@
 import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
-import IconButton from '@material-ui/core/IconButton';
 import styles from '../styles/Navbar.module.scss';
-
-import NavbarLeftMenu from './NavbarLeftMenu';
-import NavbarRightMenu from './NavbarRightMenu';
-
-import Avatar from '@material-ui/core/Avatar';
 import avatarImg from "../images/me.jpg";
+
+import { Link } from 'react-router-dom';
 
 class Navbar extends Component {
   render() {
-    const { logoutUser, user } = this.props;
+    // const { logoutUser, user } = this.props;
 
     return (
-      <div className={styles.root}>
-        <AppBar elevation={0} position="static">
-          <Toolbar className={styles.navBar}>
-            <Avatar alt="Forrest Wilkins" src={avatarImg} className={styles.avatar}/>
-            <Typography
-              className={styles.flex}
-              variant="title"
-              color="inherit"
-            >
-              <Link className={styles.name} to="/">
-                Forrest Wilkins
-              </Link>
-            </Typography>
-
-            <div>
-              <NavbarLeftMenu logoutUser={logoutUser} user={user} />
-            </div>
-
-            <IconButton aria-label="Menu">
-              <NavbarRightMenu user={user} />
-            </IconButton>
-          </Toolbar>
-        </AppBar>
+      <div>
+        <Link to='/'>
+          <img className={styles.avatar} alt="Forrest Wilkins" src={avatarImg}/>
+        </Link>
+        Test
       </div>
     );
   }
