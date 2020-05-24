55cd049069b0cb02f76da5c30e7e501ea739522d
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-05-24 16:51:48 -0400

    trix-attachment-add

--- client/src/components/Wysiwyg.js
+++ client/src/components/Wysiwyg.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-import FileBase from 'react-file-base64';
-import DefaultImg from '../images/default-img.jpg';
-
 import 'trix';
 import 'trix/dist/trix.css';
 
@@ -13,7 +10,7 @@ export class Wysywig extends Component {
     this.trixInput = React.createRef();
 
     this.state = {
-      baseImage: DefaultImg
+      baseImage: ''
     }
   }
 
@@ -21,48 +18,41 @@ export class Wysywig extends Component {
     this.trixInput.current.addEventListener("trix-change", event => {
       this.props.onChange(event.target.innerHTML); //calling custom event
     });
-    // this.trixInput.current.addEventListener("trix-attachment-add", event => {});
+    this.trixInput.current.addEventListener("trix-attachment-add", event => {
+      var attachment = event.attachment;
+      if (attachment.file) {
+        this.getBaseFile(attachment.file);
+      }
+    });
   }
 
-  setDefaultImage = () => {
-    this.setState({
-      baseImage: DefaultImg
-    });
-  };
-
   // function to capture base64 format of an image
-  getBaseFile = (files) => {
+  getBaseFile = (file) => {
+    let Base64 = require('js-base64').Base64;
     // create a local readable base64 instance of an image
     this.setState({
-      baseImage: files.base64
+      baseImage: Base64.encode(file)
     });
     let imageObj = {
       name: "base-image-" + Date.now(),
-      data: files.base64.toString()
+      data: Base64.encode(file).toString()
     };
     axios.post('/images/upload', imageObj)
       .then((data) => {
         if (data.data.success) {
           alert("Image has been successfully uploaded using base64 format");
-          this.setDefaultImage();
         }
       })
       .catch((err) => {
-        alert("Error while uploading image using base64 format")
-        this.setDefaultImage();
+        alert("Error while uploading image using base64 format");
       });
   };
 
   render() {
-    const { baseImage } = this.state;
-
     return (
       <div>
         <input type="hidden" id="trix" value={this.props.value} />
         <trix-editor input="trix" ref={this.trixInput} />
-
-        <FileBase type="file" multiple={false} onDone={this.getBaseFile} />
-        <img src={baseImage} alt="upload-img" />
       </div>
     );
   }
