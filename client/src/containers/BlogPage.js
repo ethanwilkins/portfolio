d5397ec0f158f9fdc45c201c68f8cbaf5060c744
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-06-03 13:18:13 -0400

    all_x_posts

--- client/src/containers/BlogPage.js
+++ client/src/containers/BlogPage.js
@@ -3,18 +3,33 @@ import NavbarContainer from './NavbarContainer';
 import Footer from '../components/Footer';
 import CreatePost from './CreatePost';
 import PostFeed from './PostFeed';
+import axios from 'axios';
 
 import styles from '../styles/BlogPage.module.scss';
 
 export class BlogPage extends Component {
+  state = {
+    postsSize: ''
+  };
+
+  componentDidMount = () => {
+    axios.get('/posts').then((res) => {
+      if (res.data) {
+        this.setState({postsSize: res.data.length});
+      }
+    });
+  };
+
   render() {
+    const { postsSize } = this.state;
+
     return (
       <div>
         <NavbarContainer />
         <div className={styles.blog}>
           <div className={styles.leftPane}>
             <div className="linkActive">
-              All X Posts
+              All {postsSize} Posts
             </div>
           </div>
           <div className={styles.feed}>
