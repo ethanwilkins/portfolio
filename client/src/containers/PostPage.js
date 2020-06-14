95f5d107dde08c7480be9dc4e9bb4a54fd2e7c09
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-06-14 16:29:39 -0400

    disqus

--- client/src/containers/PostPage.js
+++ client/src/containers/PostPage.js
@@ -11,6 +11,7 @@ import Footer from '../components/Footer';
 
 import styles from '../styles/PostPage.module.scss';
 
+import ReactDisqusComments from 'react-disqus-comments';
 
 class PostPage extends Component {
   state = {
@@ -72,6 +73,13 @@ class PostPage extends Component {
             body={body}
             imageData={imageData}
             timestamp={timestamp}
+          />
+          <ReactDisqusComments
+            shortname="example"
+            identifier="something-unique-12345"
+            title="Example Thread"
+            url="http://www.example.com/example-thread"
+            category_id="123456"
           />
         </div>
         <Footer />
