97db26c94cfe7bf7473cf1c82bb2de13f72aef9b
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-06-14 11:43:07 -0400

    multer_still_erratic1

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -27,7 +27,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 10
+    fileSize: 1024 * 1024 * 10 // 10 mb
   },
   fileFilter: fileFilter
 });
