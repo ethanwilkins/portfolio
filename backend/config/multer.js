432de07e1fe38b088d4e0bb477f7cd315d9f9430
ethanwilkins <ethanforrestwilkins@gmail.com>
2020-06-13 20:12:24 -0400

    multerConfig

--- backend/config/multer.js
+++ backend/config/multer.js
@@ -6,13 +6,17 @@ const storage = multer.diskStorage({
     cb(null, './uploads/');
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + file.originalname);
+    // regex extracts extension from file name
+    const re = /(?:\.([^.]+))?$/;
+    const extension = re.exec(file.originalname)[0];
+    // uses only date and extension of original file name
+    cb(null, Date.now() + extension);
   }
 });
 
 // filefilter acts as gatekeeper and allows specified file types
 const fileFilter = (req, file, cb) => {
-  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/jpg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif') {
+  if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png' || file.mimetype === 'image/gif') {
     cb(null, true);
   } else {
     // rejects storing a file
@@ -23,7 +27,7 @@ const fileFilter = (req, file, cb) => {
 const upload = multer({
   storage: storage,
   limits: {
-    fileSize: 1024 * 1024 * 5
+    fileSize: 1024 * 1024 * 10
   },
   fileFilter: fileFilter
 });
